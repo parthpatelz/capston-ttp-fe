551dd05d862463fd4977f024fa9577f09e93074a
parthpatelz <49402258+parthpatelz@users.noreply.github.com>
2024-06-11 15:31:29 -0400

    6/11/24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,28 @@ import { Component } from '@angular/core';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+
 import { RouterModule } from '@angular/router';
-import { routes } from './app.routes'; 
+import { routes } from './app.routes';
 import { RegisterComponent } from './register/register.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routes';
 import { LoginComponent } from './login/login.component';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';  
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    FooterComponent, 
-    HeaderComponent, 
+    FooterComponent,
+    HeaderComponent,
     RegisterComponent,
     LoginComponent,
     FormsModule,
     ReactiveFormsModule,
     RouterModule],
-  templateUrl: './app.component.html', 
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
